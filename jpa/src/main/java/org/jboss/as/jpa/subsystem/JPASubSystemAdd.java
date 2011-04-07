58f6f40f087f5a482e4510573d3475e7c309bc80
Scott Marlow <smarlow@redhat.com>
2011-04-07 18:52:44 -0400

    rearrange in PHASE order

--- jpa/src/main/java/org/jboss/as/jpa/subsystem/JPASubSystemAdd.java
+++ jpa/src/main/java/org/jboss/as/jpa/subsystem/JPASubSystemAdd.java
@@ -24,34 +24,25 @@ package org.jboss.as.jpa.subsystem;
 import org.jboss.as.controller.BasicOperationResult;
 import org.jboss.as.controller.ModelAddOperationHandler;
 import org.jboss.as.controller.OperationContext;
-import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.OperationResult;
 import org.jboss.as.controller.ResultHandler;
-import org.jboss.as.controller.RuntimeTask;
-import org.jboss.as.controller.RuntimeTaskContext;
-import org.jboss.as.controller.descriptions.DescriptionProvider;
-import org.jboss.as.controller.operations.common.Util;
-import org.jboss.as.controller.operations.validation.ParametersValidator;
-import org.jboss.as.controller.operations.validation.StringLengthValidator;
 import org.jboss.as.jpa.hibernate.HibernatePersistenceProviderAdaptor;
 import org.jboss.as.jpa.persistenceprovider.PersistenceProviderAdapterRegistry;
 import org.jboss.as.jpa.persistenceprovider.PersistenceProviderResolverImpl;
 import org.jboss.as.jpa.processor.JPAAnnotationParseProcessor;
 import org.jboss.as.jpa.processor.JPADependencyProcessor;
+import org.jboss.as.jpa.processor.PersistenceProviderProcessor;
 import org.jboss.as.jpa.processor.PersistenceRefProcessor;
 import org.jboss.as.jpa.processor.PersistenceUnitDeploymentProcessor;
 import org.jboss.as.jpa.processor.PersistenceUnitParseProcessor;
-import org.jboss.as.jpa.service.JPAService;
 import org.jboss.as.server.BootOperationContext;
 import org.jboss.as.server.BootOperationHandler;
 import org.jboss.as.server.deployment.Phase;
 import org.jboss.dmr.ModelNode;
-import org.jboss.msc.service.ServiceTarget;
 
-import java.util.Locale;
-
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REMOVE;
 
 /**
  * Add the JPA subsystem directive.
@@ -60,38 +51,19 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_
  *
  * @author Scott Marlow
  */
+class JPASubsystemAdd implements ModelAddOperationHandler, BootOperationHandler {
 
-class JPASubSystemAdd implements ModelAddOperationHandler, BootOperationHandler, DescriptionProvider {
+    static final JPASubsystemAdd INSTANCE = new JPASubsystemAdd();
 
-
-    static ModelNode getAddOperation(ModelNode address, ModelNode currentModel) {
-        ModelNode addOp = Util.getEmptyOperation(OPERATION_NAME, address);
-        addOp.get(CommonAttributes.DEFAULT_DATASOURCE).set(currentModel.get(CommonAttributes.DEFAULT_DATASOURCE));
-        return addOp;
-    }
-
-    static final String OPERATION_NAME = ADD;
-    static final JPASubSystemAdd INSTANCE = new JPASubSystemAdd();
-
-    private ParametersValidator modelValidator = new ParametersValidator();
-    private ParametersValidator runtimeValidator = new ParametersValidator();
-
-    private JPASubSystemAdd() {
-        modelValidator.registerValidator(CommonAttributes.DEFAULT_DATASOURCE, new StringLengthValidator(0, Integer.MAX_VALUE, false, true));
-        runtimeValidator.registerValidator(CommonAttributes.DEFAULT_DATASOURCE, new StringLengthValidator(0, Integer.MAX_VALUE, false, false));
+    private JPASubsystemAdd() {
+        //
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public OperationResult execute(final OperationContext context, final ModelNode operation, final ResultHandler resultHandler)
-        throws OperationFailedException {
-
-        modelValidator.validate(operation);
-        ModelNode defaultDSNode = operation.require(CommonAttributes.DEFAULT_DATASOURCE);
-        context.getSubModel().get(CommonAttributes.DEFAULT_DATASOURCE).set(defaultDSNode);
-        final ModelNode compensatingOperation = Util.getResourceRemoveOperation(operation.require(OP_ADDR));
+    public OperationResult execute(OperationContext context, ModelNode operation, ResultHandler resultHandler) {
 
         if (context instanceof BootOperationContext) {
 
@@ -106,32 +78,18 @@ class JPASubSystemAdd implements ModelAddOperationHandler, BootOperationHandler,
             updateContext.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_PERSISTENCE_UNIT, new PersistenceUnitParseProcessor());
             updateContext.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_PERSISTENCE_ANNOTATION, new JPAAnnotationParseProcessor());
             updateContext.addDeploymentProcessor(Phase.DEPENDENCIES, Phase.DEPENDENCIES_JPA, new JPADependencyProcessor());
-            // TODO: enable updateContext.addDeploymentProcessor(Phase.INSTALL, Phase.INSTALL_PERSISTENCE_PROVIDER, new PersistenceProviderProcessor());
-            updateContext.addDeploymentProcessor(Phase.POST_MODULE, Phase.POST_MODULE_PERSISTENCE_REF, new PersistenceRefProcessor());
+            updateContext.addDeploymentProcessor(Phase.INSTALL, Phase.INSTALL_PERSISTENCE_PROVIDER, new PersistenceProviderProcessor());
+            updateContext.addDeploymentProcessor(Phase.INSTALL, Phase.INSTALL_PERSISTENCE_REF, new PersistenceRefProcessor());
             updateContext.addDeploymentProcessor(Phase.INSTALL, Phase.INSTALL_PERSISTENTUNIT, new PersistenceUnitDeploymentProcessor());
         }
 
-        if (context.getRuntimeContext() != null) {
-            runtimeValidator.validate(operation.resolve());
-            final String dataSourceName = defaultDSNode.resolve().asString();
-            context.getRuntimeContext().setRuntimeTask(new RuntimeTask() {
-                @Override
-                public void execute(RuntimeTaskContext context) throws OperationFailedException {
-                    final ServiceTarget target = context.getServiceTarget();
-                    JPAService.addService(target, dataSourceName);
-                    resultHandler.handleResultComplete();
-                }
-            });
-        } else {
-            resultHandler.handleResultComplete();
-        }
+        final ModelNode compensatingOperation = new ModelNode();
+        compensatingOperation.get(OP).set(REMOVE);
+        compensatingOperation.get(OP_ADDR).set(operation.require(OP_ADDR));
 
+        context.getSubModel().setEmptyObject();
+        resultHandler.handleResultComplete();
         return new BasicOperationResult(compensatingOperation);
-    }
-
-    @Override
-    public ModelNode getModelDescription(Locale locale) {
-        return JPADescriptions.getSubsystemAdd(locale);
     }
 
 }
