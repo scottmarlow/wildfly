6eb7182778a391baec44e9550777532d476b86fd
Scott Marlow <smarlow@redhat.com>
2015-03-05 12:56:36 -0500

    add nosql modules for MongoDB,Cassandra drivers and simple unit tests

---
+++ nosql/src/main/java/org/jboss/as/nosql/driver/mongodb/MongoDriverService.java
@@ -0,0 +1,81 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.nosql.driver.mongodb;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
+
+/**
+ * MongoDriverService
+ *
+ * @author Scott Marlow
+ */
+public class MongoDriverService implements Service<MongoDriverService> {
+    final ConfigurationBuilder builder;
+
+    private MongoClient client;
+    private DB database;
+
+    public MongoDriverService(ConfigurationBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public void start(StartContext startContext) throws StartException {
+        try {
+            client = builder.build();
+            if(builder.getDatabase() != null) {
+                database = client.getDB(builder.getDatabase());
+            }
+
+        } catch (UnknownHostException e) {
+            throw new StartException(e);
+        }
+    }
+
+    @Override
+    public void stop(StopContext stopContext) {
+        client.close();
+        client = null;
+    }
+
+    @Override
+    public MongoDriverService getValue() throws IllegalStateException, IllegalArgumentException {
+        return this;
+    }
+
+    public MongoClient getClient() {
+        return client;
+    }
+
+    public DB getDatabase() {
+        return database;
+    }
+
+}
