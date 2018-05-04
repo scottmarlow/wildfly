1b81d024a1df02b165f8bc514f6770d823b8b94f
Gail Badner <gbadner@redhat.com>
2018-05-04 00:43:30 -0700

    Remove call to Session#beginTransaction

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/hibernate/secondlevelcache/SFSB.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/hibernate/secondlevelcache/SFSB.java
@@ -31,7 +31,6 @@ import javax.ejb.TransactionManagementType;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
@@ -89,8 +88,7 @@ public class SFSB {
 
         try {
             Session session = sessionFactory.openSession();
-            Transaction ormTransaction = session.beginTransaction(); // join the current JTA transaction
-            TransactionStatus status = ormTransaction.getStatus();
+            TransactionStatus status = session.getTransaction().getStatus();
             if(status.isNotOneOf(TransactionStatus.ACTIVE)) {
                 throw new RuntimeException("Hibernate Transaction is not active after joining Hibernate to JTA transaction: " + status.name());
             }
@@ -112,8 +110,7 @@ public class SFSB {
 
         try {
             Session session = sessionFactory.openSession();
-            Transaction ormTransaction = session.beginTransaction(); // join the current JTA transaction
-            TransactionStatus status = ormTransaction.getStatus();
+            TransactionStatus status = session.getTransaction().getStatus();
             if(status.isNotOneOf(TransactionStatus.ACTIVE)) {
                 throw new RuntimeException("Hibernate Transaction is not active after joining Hibernate to JTA transaction: " + status.name());
             }
