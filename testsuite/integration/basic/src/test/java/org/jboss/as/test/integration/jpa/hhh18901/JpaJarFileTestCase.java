b131ed44df024e81bd481e95a7af753e03a5df6d
Scott Marlow <smarlow@redhat.com>
2025-05-28 17:31:59 -0400

    Possible reproducer for WFLY-20572 on top of changes made for WFLY-20277 that contain Jakarta EE TCK test classes that shouldn't be included in a pull request
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hhh18901/JpaJarFileTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hhh18901/JpaJarFileTestCase.java
@@ -65,6 +65,13 @@ public class JpaJarFileTestCase {
         for (int looper = 1 ; looper < 5; looper++ ) {
             WebArchive war = ShrinkWrap.create(WebArchive.class, looper + "-NonTransactionalEmTestCase.war");
             war.addClasses(HttpRequest.class, SimpleServlet.class);
+            war.addClass(Employee.class);
+            war.addClass(Project.class);
+            war.addClass(PartTimeEmployee.class);
+            war.addClass(Department.class);
+            war.addClass(AbstractPersonnel.class);
+            war.addClass(MainArchiveEntity.class);
+
             // war.addAsResource(PersistenceUnitInAppClientArchiveInServerTestCase.class.getPackage(), "persistence.xml", "META-INF/persistence.xml");
             war.addAsWebInfResource(JpaJarFileTestCase.class.getPackage(), "web.xml", "web.xml");
             war.addAsManifestResource(JpaJarFileTestCase.class.getPackage(), "persistence.xml", "persistence.xml");
