7958aa2b27f35ab13531beda32ea106f36ad2185
Scott Marlow <smarlow@redhat.com>
2025-05-29 15:27:22 -0400

    Possible reproducer for WFLY-20572
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hhh18901/CdiTestBean.java
@@ -0,0 +1,61 @@
+package org.jboss.as.test.integration.jpa.hhh18901;
+
+import java.util.Calendar;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceUnit;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.transaction.Transactional;
+
+/**
+ * CdiTestBean
+ *
+ * @author Scott Marlow
+ */
+public class CdiTestBean {
+
+    @PersistenceUnit(unitName = "mainNonTXPu")
+    EntityManagerFactory emf;
+
+
+    public void createData() {
+        createTestData();
+    }
+
+    public void logTrace(String message) {
+        System.out.println(message);
+    }
+
+    java.util.Date dateId = getPKDate(2006, 04, 15);
+
+    public java.util.Date getPKDate(final int yy, final int mm, final int dd) {
+        Calendar newCal = Calendar.getInstance();
+        newCal.clear();
+        newCal.set(yy, mm, dd);
+        logTrace("getPKDate: returning date:" + newCal.getTime());
+        return newCal.getTime();
+    }
+
+    @Transactional
+    public void createTestData() {
+
+        logTrace("createTestData");
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Character[] cArray = {'a'};
+        Byte[] bArray = {(byte) 100};
+        DataTypes d1 = new DataTypes(1, true, 'a', (short) 100, 500, 300L, 50D, 1.0F,
+                cArray, bArray);
+
+        DataTypes2 d2 = new DataTypes2(dateId);
+
+        em.persist(d1);
+        em.persist(d2);
+
+        em.flush();
+
+    }
+
+}
+
--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hhh18901/JpaJarFileTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hhh18901/JpaJarFileTestCase.java
@@ -8,15 +8,19 @@ package org.jboss.as.test.integration.jpa.hhh18901;
 
 import javax.naming.NamingException;
 
+import jakarta.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+
 
 /**
  * Reproducer for https://hibernate.atlassian.net/browse/HHH-18901 based on TCK test classes currently.
@@ -43,44 +47,45 @@ public class JpaJarFileTestCase {
 
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "jarfile.jar");
         jar.addAsManifestResource(JpaJarFileTestCase.class.getPackage(), "persistence.xml", "persistence.xml");
+        jar.addClasses(CdiTestBean.class, DataTypes.class, DataTypes2.class, Grade.class);
+        jar.addAsManifestResource(new StringAsset("<beans bean-discovery-mode=\"all\"></beans>"), "beans.xml");
         ear.addAsLibrary(jar);
 
-        for (int looper = 1 ; looper < 10; looper++ ) {
-            JavaArchive clientModule = ShrinkWrap.create(JavaArchive.class,looper + "-notappclientcontainer.jar");
+        for (int looper = 1; looper < 10; looper++) {
+            JavaArchive clientModule = ShrinkWrap.create(JavaArchive.class, looper + "-notappclientcontainer.jar");
             clientModule.addClasses(JpaJarFileTestCase.class, DataTypes.class, DataTypes.class, Grade.class);
 
-            clientModule.addAsManifestResource( JpaJarFileTestCase.class.getPackage(), "application-client.xml","application-client.xml");
+            clientModule.addAsManifestResource(JpaJarFileTestCase.class.getPackage(), "application-client.xml", "application-client.xml");
             clientModule.addAsManifestResource(JpaJarFileTestCase.class.getPackage(), "persistence.xml", "persistence.xml");
             ear.addAsModule(clientModule);
         }
 
-        for (int looper = 1 ; looper < 10; looper++ ) {
-            JavaArchive ejbModule = ShrinkWrap.create(JavaArchive.class,looper + "-ejb-module.jar");
+        for (int looper = 1; looper < 10; looper++) {
+            JavaArchive ejbModule = ShrinkWrap.create(JavaArchive.class, looper + "-ejb-module.jar");
             ejbModule.addClasses(JpaJarFileTestCase.class, DataTypes.class, DataTypes.class, Grade.class);
 
             ejbModule.addAsManifestResource(JpaJarFileTestCase.class.getPackage(), "persistence.xml", "persistence.xml");
             ear.addAsModule(ejbModule);
         }
 
-        for (int looper = 1 ; looper < 10; looper++ ) {
+        for (int looper = 1; looper < 10; looper++) {
             WebArchive war = ShrinkWrap.create(WebArchive.class, looper + "-NonTransactionalEmTestCase.war");
-            war.addClasses(JpaJarFileTestCase.class,  DataTypes.class, DataTypes.class, Grade.class);
+            war.addClasses(CdiTestBean.class, JpaJarFileTestCase.class, DataTypes.class, DataTypes.class, Grade.class);
             war.addAsManifestResource(JpaJarFileTestCase.class.getPackage(), "persistence.xml", "persistence.xml");
+            war.addAsWebInfResource(new StringAsset("<beans bean-discovery-mode=\"all\"></beans>"), "beans.xml");
             ear.addAsModule(war);
         }
         return ear;
     }
 
 
+    @Inject
+    private CdiTestBean bean;
+
     @Test
     public void testEntityInMainArchive() throws NamingException {
 
-    }
-
-    @Test
-    public void testEntityInJarFileArchive() throws NamingException {
-        // JpaTestSlsb slsb = (JpaTestSlsb) new InitialContext().lookup("java:module/" + JpaTestSlsb.class.getSimpleName());
-        // slsb.testJarFileEntity();
+        bean.createData();
     }
 
 }
