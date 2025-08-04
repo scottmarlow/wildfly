ff8fec97f340a1991886087c57cca11a33634722
Scott Marlow <smarlow@redhat.com>
2025-08-04 12:36:56 -0400

    [WFLY-19554] Persistence tests should be passing now
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- jpa/subsystem/src/main/java/org/jboss/as/jpa/beanmanager/IntegratePersistenceAfterBeanDiscovery.java
+++ jpa/subsystem/src/main/java/org/jboss/as/jpa/beanmanager/IntegratePersistenceAfterBeanDiscovery.java
@@ -1,14 +1,16 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.jboss.as.jpa.beanmanager;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.Extension;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.transaction.TransactionManager;
-import jakarta.transaction.TransactionSynchronizationRegistry;
 import org.jipijapa.plugin.spi.PersistenceUnitMetadata;
 
 /**
@@ -17,42 +19,28 @@ import org.jipijapa.plugin.spi.PersistenceUnitMetadata;
  * @author Scott Marlow
  */
 public class IntegratePersistenceAfterBeanDiscovery implements Extension {
+    private final CopyOnWriteArrayList<IntegrationWithCDIBagImpl> copyOnWriteArrayList = new CopyOnWriteArrayList();
+    private volatile boolean afterBeanDiscoveryEventRanAlready = false;
 
-    private volatile PersistenceUnitMetadata persistenceUnitMetadata;
-    private volatile TransactionManager transactionManager;
-    private volatile TransactionSynchronizationRegistry transactionSynchronizationRegistry;
-    private volatile CompletableFuture<EntityManagerFactory> futureEntityManagerFactory;
     void afterBeanDiscovery(@Observes AfterBeanDiscovery event, BeanManager manager) {
+        afterBeanDiscoveryEventRanAlready = true;
         try {
-            PersistenceIntegrationWithCDI.addBeans(event, persistenceUnitMetadata, transactionSynchronizationRegistry, transactionManager, futureEntityManagerFactory);
+            for (IntegrationWithCDIBagImpl integrationWithCDIBag: copyOnWriteArrayList) {
+                new PersistenceIntegrationWithCDI().addBeans(event, integrationWithCDIBag.getPersistenceUnitMetadata(), integrationWithCDIBag);
+            }
         } catch (RuntimeException e) {
             event.addDefinitionError(e);
         }
     }
 
-    public void register(final PersistenceUnitMetadata persistenceUnitMetadata, TransactionManager transactionManager, TransactionSynchronizationRegistry transactionSynchronizationRegistry, CompletableFuture<EntityManagerFactory> futureEntityManagerFactory) {
-        this.persistenceUnitMetadata = persistenceUnitMetadata;
-        this.transactionManager = transactionManager;
-        this.transactionSynchronizationRegistry = transactionSynchronizationRegistry;
-        this.futureEntityManagerFactory = futureEntityManagerFactory;
-    }
-
-    public CompletableFuture<EntityManagerFactory> getFutureEntityManagerFactory() {
-        return futureEntityManagerFactory;
-    }
-
-    /**
-     * deploymentComplete() should be called when deployment is complete.
-     */
-    public void deploymentComplete() {
-        persistenceUnitMetadata = null;
-        transactionManager = null;
-        transactionSynchronizationRegistry = null;
-        if (futureEntityManagerFactory != null) {
-            // ensure other threads are released that are waiting for futureEntityManagerFactory to complete.
-            futureEntityManagerFactory.complete(null);
+    public IntegrationWithCDIBagImpl register(final PersistenceUnitMetadata persistenceUnitMetadata) {
+        if (afterBeanDiscoveryEventRanAlready) {
+            throw new IllegalStateException("IntegratePersistenceAfterBeanDiscovery cannot register " + persistenceUnitMetadata.getScopedPersistenceUnitName() + " as " +
+                    "jakarta.enterprise.inject.spi.AfterBeanDiscovery already ran for the deployment.");
         }
-        futureEntityManagerFactory = null;
+        final IntegrationWithCDIBagImpl integrationWithCDIBag = new IntegrationWithCDIBagImpl();
+        integrationWithCDIBag.setPersistenceUnitMetadata(persistenceUnitMetadata);
+        copyOnWriteArrayList.add(integrationWithCDIBag);
+        return integrationWithCDIBag;
     }
-
 }
